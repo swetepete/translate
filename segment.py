c59201b7993a8e6a3297df02cc14e7d9f10586ad
swetepete <100341632+swetepete@users.noreply.github.com>
2022-10-05 17:11:08 +0200

    Create segment.py

---
+++ segment.py
@@ -0,0 +1,29 @@
+import json
+import spacy
+import sys
+
+# run this script by passing the ebook title
+book_title = sys.argv[1]
+
+text = open(book_title)
+
+# this names the segments file after the ebook title with the correct suffix
+if book_title[-4:] == ".txt":
+  filename = book_title[:-4] + ".json"
+else:
+  filename = book_title + ".json"
+
+f = open(filename, "w")
+
+
+
+nlp = spacy.load("de_core_news_sm")
+
+doc = nlp(text)
+
+sentences = [sentence.text for sentence in doc.sents]
+
+# Spacy segmentation leaves NSU's (non-sentential units) next to each other as a single segment. We want to split them up.
+
+# pair each sentence with a slot for its translation
+segments = 
