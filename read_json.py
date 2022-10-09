8cc212e98ad1ea89394d05842d35e16c205da8ec
swetepete <100341632+swetepete@users.noreply.github.com>
2022-10-09 15:02:54 +0200

    Update read_json.py

--- read_json.py
+++ read_json.py
@@ -5,11 +5,25 @@ import deepl
 
 fn = sys.argv[1]
 
-sentences = json.load(open(fn))
+f = open(fn, "r+")
+
+data = json.load(f)
+
+current_loc = data["current_loc"]
+
+sentences = data["sentences"]
 
 translator = deepl.Translator(os.environ["DEEPL_API"])
 
-
-for sentence in sentences:
+for index, sentence in enumerate(sentences[current_loc:]):
+  
   input(sentence + "\n" + ("-" * os.get_terminal_size().columns))
   input(translator.translate_text(sentence, target_lang="EN-US").text + "\n" + ("-" * os.get_terminal_size().columns))
+  
+  current_loc += 1
+  data["current_loc"] = current_loc
+  f.seek(0)
+  json.dump(data, f)
+  f.truncate()
+  
+  
