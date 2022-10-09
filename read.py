da4a880be5041287fe64af32daee6daa5bb3b2e7
swetepete <100341632+swetepete@users.noreply.github.com>
2022-10-09 12:47:34 +0200

    Update read.py

--- read.py
+++ read.py
@@ -5,7 +5,7 @@ import sys
 
 nlp = spacy.load("de_core_news_sm")
 nlp.max_length = 2000000
-sentences = [sentence.text for sentence in nlp(open(sys.argv[1]).read()).sents if sentence.text.isspace()]
+sentences = [sentence.text for sentence in nlp(open(sys.argv[1]).read()).sents if not sentence.text.isspace()]
 
 
 translator = deepl.Translator(os.environ["DEEPL_API"])
